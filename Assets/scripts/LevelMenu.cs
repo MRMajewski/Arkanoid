1c83b7956d10221a3c701d1f1b1af6dd66cdcb00
MadMonk <marcin.majewski.programista@gmail.com>
2018-06-13 19:53:44 +0200

    level generator added

---
+++ Assets/scripts/LevelMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelMenu : MonoBehaviour {
+
+    [SerializeField]
+    string LevelName;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        var finished = PlayerPrefs.GetInt(LevelName + "_finished", 0) !=0;
+        GetComponent<Image>().color = finished ? Color.green : Color.white;
+
+
+        GetComponentInChildren<Text>().text = LevelName;
+        GetComponent<Button>().onClick.AddListener(ChangeScene);
+
+	}
+	
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("game");
+        PlayerPrefs.SetString("current_level", LevelName);
+    }
+
+}
